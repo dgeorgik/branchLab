3ef8951d7345cfc02df0c6947a11c0cbaadcbcd2
dgeorgik <gosha_pustovalov@mail.ru>
2023-11-30 15:01:57 +0300

    Second commit in dev branch

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Это добалено в ветке main");
         System.out.println("main-branch : Commit 1");
+        System.out.println("main-branch : Commit 2");
     }
 }
